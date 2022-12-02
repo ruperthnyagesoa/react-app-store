a6aa96d20595be1d05a34888b547dff7a9869855
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2022-12-02 13:01:32 +0300

    first commit

---
+++ src/shirts.js
@@ -0,0 +1,122 @@
+const teamList = [
+    {   id: 1,
+        team: "ARGENTINA",
+        price: 89,
+        image: "img/argentina.jpg",
+        quantity: 0
+    },
+    {
+        id: 2,
+        team: "BRAZIL",
+        price: 89,
+        image: "img/brazil.jpg",
+        quantity: 0
+    },
+    {
+        id: 3,
+        team: "USA",
+        price: 69,
+        image: "img/usa.jpg",
+        quantity: 0
+    },
+    {
+        id: 4,
+        team: "CROATIA",
+        price: 69,
+        image: "img/croatia.jpg",
+        quantity: 0
+    },
+    {
+        id: 5,
+        team: "ENGLAND",
+        price: 79,
+        image: "img/england.jpg",
+        quantity: 0
+    },
+    {
+        id: 6,
+        team: "FRANCE",
+        price: 79,
+        image: "img/france.jpg",
+        quantity: 0
+    },
+    {
+        id: 7,
+        team: "GERMANY",
+        price: 89,
+        image: "img/germany.jpg",
+        quantity: 0
+    },
+    {
+        id: 8,
+        team: "MEXICO",
+        price: 79,
+        image: "img/mexico.jpg",
+        quantity: 0
+    },
+    {
+        id: 9,
+        team: "ECUADOR",
+        price: 69,
+        image: "img/ecuador.jpg",
+        quantity: 0
+    },
+    {
+        id: 10,
+        team: "SPAIN",
+        price: 89,
+        image: "img/spain.jpg",
+        quantity: 0
+    },
+    {
+        id: 11,
+        team: "URUGUAY",
+        price: 79,
+        image: "img/uruguay.jpg",
+        quantity: 0
+    },
+    {
+        id: 12,
+        team: "SENEGAL",
+        price: 69,
+        image: "img/senegal.jpg",
+        quantity: 0
+    },
+    {
+        id: 13,
+        team: "BELGIUM",
+        price: 79,
+        image: "img/belgium.png",
+        quantity: 0
+    },
+    {
+        id: 14,
+        team: "POLAND",
+        price: 69,
+        image: "img/poland.jpg",
+        quantity: 0
+    },
+    {
+        id: 15,
+        team: "DENMARK",
+        price: 79,
+        image: "img/denmark.jpg",
+        quantity: 0
+    },
+    {
+        id: 16,
+        team: "COSTA RICA",
+        price: 69,
+        image: "img/costarica.jpg",
+        quantity: 0
+    },
+    {
+        id: 17,
+        team: "MOROCCO",
+        price: 69,
+        image: "img/morocco.jpg",
+        quantity: 0
+    }
+]
+
+export {teamList};
